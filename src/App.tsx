78a01b5117cfe2ff96830799a1dbd2d3d36cde2b
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-08-25 19:16:20 +0200

    feat(): install animejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Hero from "./components/sections/Hero";
+import Services from "./components/sections/Services";
 
 function App() {
   return (
-    <Hero />
+    <div className="w-full h-screen bg-gray-900">
+      <Hero />
+      <Services />
+    </div>
   );
 }
 
---
+++ src/components/sections/Services.tsx
@@ -0,0 +1,14 @@
+import { onScroll, animate } from "animejs";
+
+const Services = () => {
+  return (
+    <section className="w-full h-screen" id="services">
+      <div className="container mx-auto px-4 py-40 flex flex-col">
+        
+
+      </div>
+    </section>
+  );
+};
+
+export default Services;
