1acd13d2d3b860dfbfcab432d00ae3116de9bc0c
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-09-15 20:42:22 +0200

    refactor(): space btw title and tools

--- src/app/components/sections/tools/ToolSection.tsx
+++ src/app/components/sections/tools/ToolSection.tsx
@@ -3,7 +3,7 @@ import { ToolCard } from '../../ui/cards/ToolCard';
 
 export const ToolSection = () => {
   return (
-    <section className='w-full flex flex-col items-center justify-center gap-10 pb-10 relative overflow-hidden'>
+    <section className='w-full flex flex-col items-center justify-center gap-20 pb-10 relative overflow-hidden'>
       <h2 className='text-5xl font-bold text-white'>Herramientas</h2>
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10'>
         {toolsData.map((tool, index) => (
