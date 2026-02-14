077d5f9a7df9d10d10d99589ef225c7e1dc8ee81
Joaquín Caggiano <joaquincaggiano@MacBook-Pro-de-Joaquin.local>
2026-02-14 20:45:46 +0100

    feat(): add analytics of vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 import { LenisWrapper } from './components/wrapper/lenis-wrapper';
@@ -44,6 +45,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </LenisWrapper>
+        <Analytics />
       </body>
     </html>
   );
