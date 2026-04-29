29acd5b4e8482f05d59851d622065cc5c0f734d1
Moilab <moilab.web@gmail.com>
2026-04-29 15:01:43 +0200

    feat(): horizontal scroll hook

--- src/app/components/sections/SectionLayout.tsx
+++ src/app/components/sections/SectionLayout.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const SectionLayout = ({ children, className, id }: Props) => {
   return (
-    <div className={`w-full min-h-screen py-10 ${className}`} id={id}>
+    <div className={`w-full min-h-screen ${className}`} id={id}>
       <div className='container mx-auto flex flex-col items-center'>
         {children}
       </div>
